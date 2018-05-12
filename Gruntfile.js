e835a54b109b9e1b195d5e77d4a896c020c2c576
Sergio Oporto Tejerina <future_s_e_r_@hotmail.com>
2018-05-11 20:02:38 -0400

    CI-001: Added tasks for copying html files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,21 @@
 module.exports = function(grunt) { 
-	grunt.registerTask('build', function() {
-		console.log('Hello World!');
+	var config = grunt.file.readJSON(grunt.option('config') || 'config.json');
+	
+	grunt.registerTask('generatePage', function(oldPageName, newPageName) {
+		grunt.file.copy('./'+oldPageName+'.html', config.buildFolder+'/'+newPageName+'.html', {
+			process: function(files){
+					return grunt.template.process(files, {
+							data: {
+								pageTitle: config.appName
+							}
+					});
+			}
+		});
 	});
+
+	grunt.registerTask('build', [
+															'generatePage:Index:Index', 
+															'generatePage:page1:'+config.pageOneName, 
+															'generatePage:page2:'+config.pageTwoName]);
+	
 };
