d2b684403b83f5c5497e373232034700e93b85a2
choquemeneses7 <veimar.xd@hotmail.com>
2018-05-19 23:15:48 -0400

    develop: Added menu component and routing files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent }      from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
+const routes: Routes = [{path: 'menu', component: MenuComponent}];
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]	
+})
+export class AppRoutingModule { }
+export const routingComponents = [MenuComponent]
