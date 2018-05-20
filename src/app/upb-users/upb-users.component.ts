0a59f67c1ad83ffbccee2c46c9cb4e8dc45a55e0
Elionor Cordova <elicg2012@hotmail.com>
2018-05-20 19:07:30 -0400

    SE-001: Create cards for every user with its data profile

--- src/app/upb-users/upb-users.component.ts
+++ src/app/upb-users/upb-users.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import data from '../../../data.json';
 
 @Component({
   selector: 'app-upb-users',
   templateUrl: './upb-users.component.html',
   styleUrls: ['./upb-users.component.css']
 })
-export class UpbUsersComponent implements OnInit {
+export class UpbUsersComponent implements OnInit 
+{
+  users = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.users = data.users;
   }
 
 }
