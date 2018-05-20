4eaaa65dcbca132b708cab16d194b6c3eb50bc8d
Sergio Oporto Tejerina <future_s_e_r_@hotmail.com>
2018-05-20 11:15:05 -0400

    SE-001: Added a route for the upb-users component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent }      from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { UpbUsersComponent } from './upb-users/upb-users.component';
 
-const routes: Routes = [{path: 'menu', component: MenuComponent}];
+const routes: Routes = [
+  {path: '', component: MenuComponent},
+  {path: 'upb', component: UpbUsersComponent}
+];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]	
