5d80dff4164564d737a6ad1693260ba818708c48
Sergio Oporto Tejerina <future_s_e_r_@hotmail.com>
2018-05-22 03:30:05 -0400

    develop: Added condition to access page two link

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as config from '../../../config.json';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class MenuComponent {
+  readonly pageTwoEnabled : boolean = (<any>config).enablePageTwoLink;
 }
